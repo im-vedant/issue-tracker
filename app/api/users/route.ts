dee45b63230e12b36767b05e7c5616529c5dfabc
im-vedant <194vedantgupta@gmail.com>
2024-04-05 13:47:52 +0530

    Update route.ts

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,7 +1,8 @@
 import prisma from "@/prisma/db";
 import { NextRequest, NextResponse } from "next/server";
-
+import { cookies } from 'next/headers'
 export  async function GET(req : NextRequest){
+    cookies()
    const users=await  prisma.user.findMany(
         {
             orderBy : {name : 'asc'}
